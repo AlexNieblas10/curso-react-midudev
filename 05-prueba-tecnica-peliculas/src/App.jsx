4d4a363f96f19d9317d53b51a7aa1c851bd0cfa0
AlexNieblas10 <elalexnieblas@gmail.com>
2023-02-22 15:19:53 -0700

    fin 5to proyecto

--- 05-prueba-tecnica-peliculas/src/App.jsx
+++ 05-prueba-tecnica-peliculas/src/App.jsx
@@ -2,19 +2,32 @@ import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 import "./app.css";
 import { useSearch } from "./hooks/useSearch";
+import { useCallback, useState } from "react";
+import debounce from "just-debounce-it";
 
 export function App() {
+  const [sort, setSort] = useState(false)
   const { value, setValue, error } = useSearch()
-  const { movies, loading, getMovies } = useMovies({ value });
+  const { movies, loading, getMovies } = useMovies({ value, sort });
+
+  const debounceGetMovies = useCallback(
+    debounce(value => {
+      getMovies({ value })
+    }, 300), [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    getMovies()
+    getMovies(value)
   }
 
   const handleChange = (e) => {
-    setValue(e.target.value)
+    const newSearch = e.target.value
+    setValue(newSearch)
+    debounceGetMovies(newSearch)
+  }
 
+  const handleSort = () => {
+    setSort(!sort)
   }
 
   return (
@@ -22,13 +35,14 @@ export function App() {
       <h1>Busca aqui tu pelicula favorita</h1>
       <form onSubmit={handleSubmit}>
         <input onChange={handleChange} value={value} name="query" type="text" placeholder="Harry Potter, Avengers..." />
+        <input type="checkbox" onClick={handleSort} />
         <button>Buscar</button>
         {error && <p>{error}</p>}
       </form>
 
       <main>
-        {loading ?<p>Cargando...</p> : <Movies movies={movies} />}
-        
+        {loading ? <p>Cargando...</p> : <Movies movies={movies} />}
+
 
       </main>
     </div>
--- 05-prueba-tecnica-peliculas/src/hooks/useMovies.js
+++ 05-prueba-tecnica-peliculas/src/hooks/useMovies.js
@@ -1,29 +1,39 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { searchMovies } from "../services/movies";
 
-export function useMovies({ value }) {
+export function useMovies({ value, sort }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null)
   const previousSearch = useRef(value)
 
-  const getMovies = async () => {
-    if (value === previousSearch.current) return
-    
-    try{
-      setLoading(true)
-      previousSearch.current = value
-      const newMovies = await searchMovies({ value });
-      setMovies(newMovies);
-    }
-    catch(e){
-      setError(e.message)
-      setLoading(false)
-    }
-    finally{
-      setLoading(false)
-    }
-  };
+  const getMovies = useMemo(() => {
+    return async ({ value }) => {
+      if (value === previousSearch.current) return
 
-  return { movies, getMovies, loading };
+      try {
+        setLoading(true)
+        previousSearch.current = value
+        const newMovies = await searchMovies({ value });
+        setMovies(newMovies);
+      }
+      catch (e) {
+        setError(e.message)
+        setLoading(false)
+      }
+      finally {
+        setLoading(false)
+      }
+    };
+  }, [])
+
+  const sortMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+
+
+  return { movies: sortMovies, getMovies, loading };
 }
